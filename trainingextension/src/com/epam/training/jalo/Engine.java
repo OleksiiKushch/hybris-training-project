ea5b78cc99f0c4dc502902d2816a62235e44bb4d
Oleksii Kushch <Oleksii_Kushch@epam.com>
2022-07-16 00:12:46 +0300

    Task1 (HybrisMP-DataModeling)

---
+++ trainingextension/src/com/epam/training/jalo/Engine.java
@@ -0,0 +1,34 @@
+package com.epam.training.jalo;
+
+import de.hybris.platform.jalo.Item;
+import de.hybris.platform.jalo.JaloBusinessException;
+import de.hybris.platform.jalo.SessionContext;
+import de.hybris.platform.jalo.type.ComposedType;
+import org.apache.log4j.Logger;
+
+public class Engine extends GeneratedEngine
+{
+	@SuppressWarnings("unused")
+	private static final Logger LOG = Logger.getLogger( Engine.class.getName() );
+	
+	@Override
+	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes) throws JaloBusinessException
+	{
+		// business code placed here will be executed before the item is created
+		// then create the item
+		final Item item = super.createItem( ctx, type, allAttributes );
+		// business code placed here will be executed after the item was created
+		// and return the item
+		return item;
+	}
+
+	@Override
+	public Integer getNumberOfPistons(SessionContext ctx) {
+		return null;
+	}
+
+	@Override
+	public void setNumberOfPistons(SessionContext ctx, Integer value) {
+
+	}
+}
